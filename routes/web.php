ac67d8dd672b6e253fb19fbed2b03894baaced18
ariopfp <ario.pfp@gmail.com>
2024-05-12 18:19:20 +0800

    home page

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,19 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('login');
 });
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('admin/dashboard', function () {
+    return view('admin/page/home');
+});
+
+Route::get('/home', function () {
+    return view('restaurant/main');
+});
+
+Route::get('/salah', function () {
+    return view('restaurant/salah');
+});
